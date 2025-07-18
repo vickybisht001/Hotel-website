64c30b479ddf8d3ca8f485b2e94c928942d3fe34
JITENDRA SINGH BISHT <152790288+vickybisht001@users.noreply.github.com>
2025-07-18 12:21:16 +0530

    Update page.js

--- app/room/page.js
+++ app/room/page.js
@@ -126,8 +126,8 @@ const Room = () => {
       </div>
 
       <div className="service-sectionn text-center py-10 px-4 sm:px-6 lg:px-10">
-        <span className="text-[#c5a880] text-2xl">StayHotel Service</span>
-        <h1 className="text-3xl sm:text-4xl my-2 font-semibold">Explore Our Hotel Services</h1>
+        <span className="text-[#c5a880] md:text-2xl">StayHotel Service</span>
+        <h1 className="text-2xl md:text-4xl my-2 font-semibold">Explore Our Hotel Services</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
           {services.map((service) => (
